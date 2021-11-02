f2dadf88c79945796585eefa579f576ee6672b1b
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-02 18:17:46 -0400

    Added the path to the stats page on routes

--- routes/html.js
+++ routes/html.js
@@ -10,3 +10,10 @@ router.get("/", (req, res) => {
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname + "/../public/exercise.html"));
 });
+
+// Get function for stats
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../public/stats.html"))
+})
+
+module.exports = router;
