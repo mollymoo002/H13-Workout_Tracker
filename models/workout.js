439cd92bd3c43c1a105dff3ae01d3c4392659303
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-02 16:24:38 -0400

    wrapped the exercises in workout.js to make it an array

--- models/workout.js
+++ models/workout.js
@@ -3,23 +3,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for the workout"
-  },
-  duration: {
-    type: Number,
-    required: "Enter the length of the workout"
-  },
-  distance: {
-    type: Number,
-    required: "Enter the distance of the workout"
-  },
   date: {
     type: Date,
     default: Date.now
-  }
+  },
+  exercises: [
+    {
+        type: {
+            type: String,
+            trim: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter a name for the workout"
+        },
+        duration: {
+            type: Number,
+            required: "Enter the length of the workout"
+        },
+        distance: {
+            type: Number,
+            required: "Enter the distance of the workout"
+        },
+    },
+  ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,12 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+
+router.post("/api/workout", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
