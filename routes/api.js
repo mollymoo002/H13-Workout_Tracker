a5af80763e2e2c76a8339fe317d99942606809bc
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-02 17:38:44 -0400

    added the get and post function for the workout

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,28 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+// Get function for the workout
+router.get("/api/workout", (req, res) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {$sum: "$exercises.duration"},
+      },
+    },
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// post route for workouts
 router.post("/api/workout", ({ body }, res) => {
     Workout.create(body)
-      .then(dbTransaction => {
-        res.json(dbTransaction);
+      .then(dbWorkout => {
+        res.json(dbWorkout);
       })
       .catch(err => {
         res.status(400).json(err);
